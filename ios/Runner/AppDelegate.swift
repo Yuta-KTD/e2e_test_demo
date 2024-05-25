0932d79d5be7e06ec19e1c153c80bb70d581d2db
Yuta-KTD <yuta.katada.1@gmail.com>
2024-05-25 23:18:35 +0900

    dart-define考慮して修正

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if let googleMapApiKey = Bundle.main.infoDictionary?[“Google Maps API Key”] as? String {
+      GMSServices.provideAPIKey(googleMapApiKey)
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,29 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MapScreen extends StatefulHookConsumerWidget {
+import 'location/location_provider.dart';
+
+class MapScreen extends HookConsumerWidget {
   const MapScreen({super.key});
-
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _MapScreenState();
-}
-
-class _MapScreenState extends ConsumerState<MapScreen> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final locationAsyncValue = ref.watch(locationProvider);
     const tokyoStation = LatLng(35.681236, 139.767125);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Map Screen'),
       ),
-      body: const GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: tokyoStation,
-          zoom: 15,
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: switch (locationAsyncValue) {
+              AsyncData(:final value) => value != null
+                  ? LatLng(value.latitude, value.longitude)
+                  : tokyoStation,
+              AsyncError() => tokyoStation,
+              _ => tokyoStation,
+            },
+            zoom: 15,
+          ),
+          myLocationEnabled: true,
         ),
-        // myLocationEnabled: true,
       ),
     );
   }
